2c3aa8ddd1025f842268c0f4ca71d458351de452
tunghng <lehoangtung081003@gmail.com>
2022-11-24 07:04:03 +0700

    this is commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { Container, Row } from "reactstrap";
+import { React, useRef, useEffect } from "react";
+
 import { NavLink } from "react-router-dom";
 import "./header.scss";
+
+import { motion } from "framer-motion";
+
 import logo from "../../assets/images/eco-logo.png";
 import user_icon from "../../assets/images/user-icon.png";
-import { motion } from "framer-motion";
+
+import { Container, Row } from "reactstrap";
 
 const nav__link = [
     {
@@ -22,59 +26,90 @@ const nav__link = [
 ];
 
 const Header = () => {
+    const headerRef = useRef(null);
+
+    const stickyHeaderFunc = () => {
+        window.addEventListener("scroll", () => {
+            if (
+                document.body.scrollTop > 80 ||
+                document.documentElement.scrollTop > 80
+            ) {
+                headerRef.current.classList.add("sticky__header");
+            } else {
+                headerRef.current.classList.remove("sticky__header");
+            }
+        });
+    };
+
+    useEffect(() => {
+        stickyHeaderFunc();
+
+        return () => window.removeEventListener("scroll", stickyHeaderFunc);
+    });
+
     return (
-        <Container>
-            <Row>
-                <div className="nav__wrapper">
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                        <div>
-                            <h1>Multimart</h1>
-                            <p>Since 1995</p>
+        <header className="header" ref={headerRef}>
+            <Container>
+                <Row>
+                    <div className="nav__wrapper">
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
+                            <div>
+                                <h1>Multimart</h1>
+                                <p>Since 1995</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="navigation">
-                        <ul className="menu">
-                            {nav__link.map((item, index) => (
-                                <li className="nav__item">
-                                    <NavLink
-                                        to={item.path}
-                                        className={(navClass) =>
-                                            navClass.isActive ? "nav__active" : ""
-                                        }
-                                    >
-                                        {item.display}
-                                    </NavLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                        <div className="navigation">
+                            <ul className="menu">
+                                {nav__link.map((item, index) => (
+                                    <li className="nav__item">
+                                        <NavLink
+                                            to={item.path}
+                                            className={(navClass) =>
+                                                navClass.isActive
+                                                    ? "nav__active"
+                                                    : ""
+                                            }
+                                        >
+                                            {item.display}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
 
-                    <div className="nav__icons">
-                        <span className="fav__icon">
-                            <i class="ri-heart-line"></i>
-                            <span className="badge">1</span>
-                        </span>
+                        <div className="nav__icons">
+                            <span className="fav__icon">
+                                <i class="ri-heart-line"></i>
+                                <span className="badge">1</span>
+                            </span>
+
+                            <span className="cart__icon">
+                                <i class="ri-shopping-bag-line"></i>
+                                <span className="badge">1</span>
+                            </span>
+
+                            <span>
+                                <motion.img
+                                    whileTap={{ scale: 1.2 }}
+                                    src={user_icon}
+                                    alt="user icon"
+                                />
+                            </span>
+
+                            <div className="mobile__menu">
+                            <span>
+                                <i class="ri-menu-line"></i>
+                            </span>
+                        </div>
+                        </div>
+
                         
-                        <span className="cart__icon">
-                            <i class="ri-shopping-bag-line"></i>
-                            <span className="badge">1</span>
-                        </span>
-                        
-                        <span>
-                            <motion.img whileTap={{ scale: 1.2}} src={user_icon} alt="user icon" />
-                        </span>
                     </div>
-
-                    <div className="mobile__menu">
-                        <span>
-                            <i class="ri-menu-line"></i>
-                        </span>
-                    </div>
-                </div>
-            </Row>
-        </Container>
+                </Row>
+            </Container>
+        </header>
     );
 };
 
